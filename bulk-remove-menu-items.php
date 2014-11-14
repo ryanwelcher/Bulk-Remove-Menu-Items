83892427b5f379644f280965a49f8eb95379aba5
Ryan Welcher <me@ryanwelcher.com>
2014-11-14 13:59:05 -0500

    First Commit

---
+++ bulk-remove-menu-items.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Plugin Name: Bulk Remove Menu Items
+ * Plugin URI: 
+ * Description: Select and remove many items at once from the built in WordPress menu system.
+ * Version: 1.0.0
+ * Author: Ryan Welcher
+ * Author URI: http://www.ryanwelcher.com
+ * License: GPL2
+ *
+ * Copyright 2014  Ryan Welcher  (email : me@ryanwelcher.com)
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as 
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+//stop directly accessing this file
+defined('ABSPATH') or die("No script kiddies please!");
+
+/**
+ * main class
+ *
+ * 
+ */
+class RW_Bulk_Remove_Menu_Items {
+
+	/**
+	 * Sets up the hooks
+	 * @return void
+	 */
+	public function init() {
+		add_filter('wp_edit_nav_menu_walker', array($this, 'filter_wp_edit_nav_menu_walker') );
+		add_action('admin_enqueue_scripts', array( $this, 'action_admin_enqueue_scripts') );
+	}
+
+
+	/**
+	 * Filter the Walker class that will be used on nav-menus.php admin page
+	 * @param  string $walker_class The name of the Walker class to be used
+	 * @return string               The name of the Walker class to be used
+	 */
+	public function filter_wp_edit_nav_menu_walker( $walker_class ) {
+		//look in nav-menu.php for this class
+		$new_walker = 'Walker_Nav_Menu_Edit_Advanced';
+		return $new_walker; //$name_of_walker_class
+	}
+
+
+	/**
+	 * Loads our scripts ony on the nav-menus.php page
+	 *
+	 * Looks for SCRIPT_DEBUG to allow for proper debugging if needed.
+	 * 
+	 * @param  string $hook The name of the admin page file
+	 * @return void
+	 */
+	public function action_admin_enqueue_scripts( $hook ) {
+		if('nav-menus.php' == $hook) {
+			$path = ( defined( 'SCRIPT_DEBUG') && SCRIPT_DEBUG ) ? plugin_dir_url( __FILE__ ) . '/js/brmi-admin.js' : plugin_dir_url( __FILE__ ) . '/js/brmi-admin.min.js';
+			wp_enqueue_script( 'awpm_admin_scripts', $path );
+			wp_localize_script( 'awpm_admin_scripts', 'BRMI_TEXTS', array( 'button_text' => __('Remove Menu Items','awpm') ) );
+		}
+	}
+}
+
+
+$rw_bulk_remove_items = new RW_Bulk_Remove_Menu_Items();
+$rw_bulk_remove_items->init();
+
+require plugin_dir_path( __FILE__ ) . 'classes/class-walker-nav-menu-edit-advanced.php';
---
+++ js/brmi-admin.js
@@ -0,0 +1,86 @@
+var RW_BRMI;
+
+(function($){
+
+	RW_BRMI = {
+
+		/**
+		 * Init
+		 */
+		init: function() {
+
+			this.generateButtonMarkup();
+			this.addInteractivity();
+		},
+
+		/**
+		 * Generates the remove button and adds it to the html
+	
+		 */
+		generateButtonMarkup : function() {
+			var div, button;
+			div = $('<div/>',{
+				'class' : 'publishing-action'
+			});
+
+			button = $('<input/>',{
+				'class' : 'button button-primary remove-checked-items',
+				'value' : BRMI_TEXTS.button_text,
+				'style'	: 'margin-right:10px;',
+				'disabled' : 'disabled'
+			});
+			div.append( button  );
+
+			$('.major-publishing-actions').append( div );
+		},
+
+		/**
+		 * Recursively checks child items for checked menu item
+		 * @todo Currently only works on top level items
+		 */
+		
+		addInteractivity : function() {
+			var that = this;
+
+			$('#menu-to-edit').on('change','.awpm-check', function(){
+				var parent_li = $(this).parents('li'),
+				classes = parent_li.attr('class').split(" ");
+				if( $.inArray( 'menu-item-depth-0', classes ) !== -1 ) {
+					$.each( parent_li.nextAll(), function( index, element ){
+						if( $(element).hasClass( 'menu-item-depth-0' ) ) {
+							return false;
+						}else{
+							$(element).find('.awpm-check').prop('checked', true);
+						}
+					});
+				}
+				that.setButtonStatus();
+			});
+
+			$(document).on('click', '.remove-checked-items', function(e){
+				e.preventDefault();
+				$.each( $('.awpm-check:checked'), function( index, element) {
+					$(element).parents('li').find('a.item-delete').trigger('click');
+				});
+
+				$('.remove-checked-items').prop('disabled', true);
+			});
+		},
+
+		/**
+		 * Enable/disable the button based on checked items
+		 */
+		setButtonStatus : function(){
+			var button = $('.remove-checked-items');
+
+			if( $('.awpm-check:checked').length > 0 ) {
+				button.prop('disabled', false);
+			}else{
+				button.prop('disabled', true);
+			}
+		},
+	};
+
+	//start the show
+	jQuery(document).ready(function($) { RW_BRMI.init(); });
+})(jQuery);
